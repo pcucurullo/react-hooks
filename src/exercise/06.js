41e7ab3bcbaa50024d31128657c090b5e79b55a0
Paola Cucurullo <paolacucurullo@packlink.com>
2021-02-10 18:20:29 +0100

    pokemon with state

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,37 +2,36 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
 import {PokemonForm, fetchPokemon, PokemonInfoFallback, PokemonDataView} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = React.useState(null);
   const [error, setError] = React.useState(null);
+  const [status, setStatus] = React.useState('idle');
 
   React.useEffect(() => {
     if (!pokemonName || pokemonName === '') return;
-    setPokemon(null);
+    setStatus('pending');
 
     fetchPokemon(pokemonName).then(
-        pokemonData => setPokemon(pokemonData),
-        error => setError(error)
+        pokemonData => {
+          setPokemon(pokemonData);
+          setStatus('resolved');
+        },
+        error => {
+          setError(error);
+          setStatus('rejected');
+        }
       )
   }, [pokemonName]);
 
-  if (error) {
-    return (
-      <div role="alert">
-        There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
-      </div>
-    )
-  } else if (!pokemonName) {
+  if (status === 'rejected') {
+    return <div role="alert">There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre></div>
+  } else if (status === 'idle') {
     return 'Submit a pokemon'
-  } else if (!pokemon) {
+  } else if (status === 'pending') {
     return <PokemonInfoFallback name={pokemonName} />
-  } else {
+  } else if (status === 'resolved') {
     return <PokemonDataView pokemon={pokemon} />
   }
 }
