8cbfa4055c97155f9f6ca439560363af7814fa09
Paola Cucurullo <paolacucurullo@packlink.com>
2021-02-11 12:06:08 +0100

    error boundary

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -27,13 +27,38 @@ function PokemonInfo({pokemonName}) {
   }, [pokemonName]);
 
   if (status === 'rejected') {
-    return <div role="alert">There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre></div>
+    // to be handled by Error Boundary
+    throw error;
   } else if (status === 'idle') {
     return 'Submit a pokemon'
   } else if (status === 'pending') {
     return <PokemonInfoFallback name={pokemonName} />
   } else if (status === 'resolved') {
     return <PokemonDataView pokemon={pokemon} />
+  }
+}
+
+function ErrorComponent({error}) {
+  return (<div role="alert">There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre></div>)
+}
+
+class ErrorBoundary extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = { error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { error };
+  }
+
+  render() {
+    const { error } = this.state;
+    if (error) {
+      return <this.props.FallbackComponent error={error} />;
+    }
+
+    return this.props.children;
   }
 }
 
@@ -49,7 +74,9 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+        <ErrorBoundary FallbackComponent={ErrorComponent}>
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
       </div>
     </div>
   )
